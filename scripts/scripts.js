28be6415f8c185345df02cbcb42feaa9fc56d6d6
Vladislav Popov <mrkomnot@yandex.ru>
2021-11-29 19:21:14 +0500

    отправка формы через js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+// Открытие/закрытие модального окна.
+
 const editBtn = document.querySelector(".profile__edit-btn");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".modal__close-btn");
@@ -20,6 +22,8 @@ modalOverlay.addEventListener("click", closeModal);
 Из всего кода не понял зачем вот такие стрелочки (=>).
 Можете подсказать где почитать про них?)
 Заранее спасибо.
+
+UPD: +- понял что эти стрелочки сокращают количество кода.
 */
 
 const heart = Array.from(document.querySelectorAll(".element__heart"));
@@ -29,3 +33,33 @@ heart.forEach((element) => {
     element.classList.toggle("element__heart_active");
   });
 });
+
+// Сохранение изменений
+
+let formElement = document.querySelector(".modal__submit");
+let nameInput = document.querySelector(".modal__name");
+let jobInput = document.querySelector(".modal__description");
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  let newName = nameInput.value;
+  let newJob = jobInput.value;
+  // Выберите элементы, куда должны быть вставлены значения полей
+  let profName = document.querySelector(".profile__title");
+  let profJob = document.querySelector(".profile__subtitle");
+  // Вставьте новые значения с помощью textContent
+  profName.textContent = newName;
+  profJob.textContent = newJob;
+
+  closeModal();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", formSubmitHandler);
