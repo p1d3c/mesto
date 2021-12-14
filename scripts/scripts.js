ef8cd0bb1ea00d9ffd60ea5b5d1f5dce9fb96c86
Vladislav Popov <mrkomnot@yandex.ru>
2021-12-14 20:30:12 +0500

    refactoring js code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,30 @@
 const editBtn = document.querySelector('.profile__edit-btn');
 const addBtn = document.querySelector('.profile__add-btn');
 
-const modal = document.querySelector('.modal');
-const mesto = document.querySelector('.new-mesto');
-const imgPopup = document.querySelector('.image-popup');
+const profilePopup = document.querySelector('.popup_type_edit');
+const addCardPopup = document.querySelector('.popup_type_add');
+const imgPopup = document.querySelector('.popup_type_img');
 
-const modalCloseBtn = document.querySelector('.modal__close-btn');
-const mestoCloseBtn = document.querySelector('.new-mesto__close-btn');
-const imgCloseBtn = document.querySelector('.image-popup__close-btn');
+const popupCloseBtn = document.querySelectorAll('.popup__close-btn');
 
-const modalOverlay = document.querySelector('.modal__overlay');
-const mestoOverlay = document.querySelector('.new-mesto__overlay');
-const imgOverlay = document.querySelector('.image-popup__overlay');
+const profName = document.querySelector('.profile__title');
+const profJob = document.querySelector('.profile__subtitle');
+const profilePopupName = document.querySelector('.popup__input_type_name');
+const profilePopupJob = document.querySelector('.popup__input_type_job');
 
-let profName = document.querySelector('.profile__title');
-let profJob = document.querySelector('.profile__subtitle');
-let modalName = document.querySelector('.modal__input_type_name');
-let modalDesc = document.querySelector('.modal__input_type_job');
+const editFormElement = document.querySelector('.popup__form_type_edit');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
 
-let modalFormElement = document.querySelector('.modal__form');
-let nameInput = document.querySelector('.modal__input_type_name');
-let jobInput = document.querySelector('.modal__input_type_job');
+const addFormElement = document.querySelector('.popup__form_type_add');
+const titleInput = document.querySelector('.popup__input_type_title');
+const linkInput = document.querySelector('.popup__input_type_image');
 
-const mestoFormElement = document.querySelector('.new-mesto__form');
-let titleInput = document.querySelector('.new-mesto__input_type_name');
-let linkInput = document.querySelector('.new-mesto__input_type_image')
+const template = document.querySelector('#temp').content;
+const cardList = document.querySelector('.elements');
+
+const showImage = imgPopup.querySelector('.popup__image');
+const showCaption = imgPopup.querySelector('.popup__caption');
 
 const initialCards = [
   {
@@ -53,105 +53,98 @@ const initialCards = [
   }
 ];
 
-window.onload = () => {
-  initialCards.forEach((el) => {
-    const template = document.querySelector('#temp').content;
-    const element = template.querySelector('.element').cloneNode(true);
+window.onload = renderCards();
 
-    element.querySelector('.element__image').src = el.link;
-    element.querySelector('.element__title').textContent = el.name;
-
-    element.querySelector('.element__heart').addEventListener('click', (evt) => {
-      evt.target.classList.toggle('element__heart_active');
-    })
-
-    element.querySelector('.element__button').addEventListener('click', (evt) => {
-      console.log(evt.target);
-      evt.target.parentElement.remove();
-    } )
-
-    element.querySelector('.element__image').addEventListener('click', openModal);
-
-    document.querySelector('.elements').append(element);
-  })
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-function closeModal(evt) {
-  if (evt.target.classList.value === 'modal__close-btn' || evt.target.classList.value === 'modal__form' || evt.target.classList.value === 'modal__overlay') {
-    modal.classList.remove('modal_active');
-  } else if (evt.target.classList.value === 'new-mesto__close-btn' || evt.target.classList.value === 'new-mesto__form' || evt.target.classList.value === 'new-mesto__overlay') {
-    mesto.classList.remove('new-mesto_active');
-  } else if (evt.target.classList.value === 'image-popup__close-btn' || evt.target.classList.value === 'image-popup__overlay') {
-    imgPopup.classList.remove('image-popup_active');
-  }
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function openModal(evt) {
-  if (evt.target.name === 'edit') {
-    modal.classList.add('modal_active');
-    modalName.value = profName.textContent;
-    modalDesc.value = profJob.textContent;
-  } else if (evt.target.name === 'add') {
-    mesto.classList.add('new-mesto_active');
-    titleInput.value = '';
-    linkInput.value = '';
-  } else if (evt.target.classList.value === 'element__image') {
-    imgPopup.classList.add('image-popup_active');
-    let img = document.querySelector('.image-popup__image');
-    let text = document.querySelector('.image-popup__title');
-    img.src = evt.target.src;
-    text.textContent = evt.target.parentElement.querySelector('.element__title').textContent;
-  }
-}
-
-function formSubmitHandler (evt) {
+function submitFormHandler (evt) {
   evt.preventDefault();
 
   profName.textContent = nameInput.value;
   profJob.textContent = jobInput.value;
 
-  closeModal(evt);
+  closePopup(profilePopup);
 }
 
-function addElement (evt) {
+function fillProfilePopup() {
+  profilePopupName.value = profName.textContent;
+  profilePopupJob.value = profJob.textContent;
+  openPopup(profilePopup);
+}
+
+function fillAddCardPopup() {
+  titleInput.value = '';
+  linkInput.value = '';
+  openPopup(addCardPopup);
+}
+
+function fillImgPopup(el) {
+  showImage.src = el.link;
+  showImage.alt = el.name;
+  showCaption.textContent = el.name;
+  openPopup(imgPopup);
+}
+
+function delCard(el) {
+  el.remove();
+}
+
+function likeCard(el) {
+  el.classList.toggle('element__heart_active');
+}
+
+function createCard(el) {
+  const elementCard = template.querySelector('.element').cloneNode(true);
+  const img = elementCard.querySelector('.element__image');
+  const title = elementCard.querySelector('.element__title');
+  const likeBtn = elementCard.querySelector('.element__heart');
+  const delBtn = elementCard.querySelector('.element__button');
+  img.src = el.link;
+  title.textContent = el.name;
+
+  delBtn.addEventListener('click', () => delCard(elementCard))
+  likeBtn.addEventListener('click', () => likeCard(likeBtn));
+  img.addEventListener('click', ()  => fillImgPopup(el));
+  return elementCard;
+}
+
+function pasteCard(el) {
+  const elementCard = createCard(el);
+  cardList.append(elementCard);
+}
+
+function pasteNewCard(el) {
+  const elementCard = createCard(el);
+  cardList.prepend(elementCard);
+}
+
+function addNewCard(evt) {
   evt.preventDefault();
-
-  if (linkInput.value === '' || titleInput.value === '') {
-    closeModal(evt);
-  } else {
-    const template = document.querySelector('#temp').content;
-    const element = template.querySelector('.element').cloneNode(true);
-
-    element.querySelector('.element__image').src = linkInput.value;
-    element.querySelector('.element__title').textContent = titleInput.value;
-    document.querySelector('.elements').prepend(element);
-
-    element.querySelector('.element__heart').addEventListener('click', (evt) => {
-      evt.target.classList.toggle('element__heart_active');
-    })
-
-    element.querySelector('.element__button').addEventListener('click', (evt) => {
-      console.log(evt.target);
-      evt.target.parentElement.remove();
-    })
-
-    element.querySelector('.element__image').addEventListener('click', openModal);
-
-    closeModal(evt);
-  }
+  const newCard = {
+    name: titleInput.value,
+    link: linkInput.value
+  };
+  pasteNewCard(newCard);
+  closePopup(addCardPopup);
 }
 
-editBtn.addEventListener('click', openModal);
-addBtn.addEventListener('click', openModal);
+function renderCards() {
+  initialCards.forEach((el) => pasteCard(el));
+}
 
-modalCloseBtn.addEventListener('click', closeModal);
-modalOverlay.addEventListener('click', closeModal);
+popupCloseBtn.forEach((btn) => {
+  const closestPopup = btn.closest('.popup');
+  btn.addEventListener('click', () => closePopup(closestPopup));
+});
 
-mestoCloseBtn.addEventListener('click', closeModal);
-mestoOverlay.addEventListener('click', closeModal);
+editBtn.addEventListener('click', fillProfilePopup);
+addBtn.addEventListener('click', fillAddCardPopup);
 
-imgCloseBtn.addEventListener('click', closeModal);
-imgOverlay.addEventListener('click', closeModal);
-
-modalFormElement.addEventListener('submit', formSubmitHandler);
-mestoFormElement.addEventListener('submit', addElement);
+editFormElement.addEventListener('submit', submitFormHandler);
+addFormElement.addEventListener('submit', addNewCard);
